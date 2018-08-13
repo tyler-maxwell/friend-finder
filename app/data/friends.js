43920ef3cae7a3fd6727e5624aedf59c307a7a47
Tyler Maxwell <tylermaxwell1990@gmail.com>
2018-08-13 16:18:58 -0400

    html done

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friendsArray = [
+    {
+      name: "Tyler",
+      photo: "https://i.imgur.com/hkmzvBz.jpg",
+      scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  
+    }
+];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
+  
